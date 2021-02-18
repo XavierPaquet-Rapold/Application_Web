619b90089c0219223996bf0035edd2a2d3991438
Xavier Paquet-Rapold <1928107@bdeb.qc.ca>
2021-02-18 18:07:48 -0500

    Création de connexion et début de la page panier

--- Projet_web/backend/server.js
+++ Projet_web/backend/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var http = require('http');
 var mysql = require('mysql');
 var app = express();
+var session = require('express-session');
+var path = require('path');
 var bodyParser = require('body-parser');
 var dateFormat = require('dateformat');
 var now = new Date();
@@ -14,9 +16,15 @@ var server = app.listen(4000, function () {
     console.log("serveur fonctionne sur 4000");
 });
 
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 module.exports = app;
-
+app.use(bodyParser.json());
 app.use( express.static( "views" ) );
 
 app.set('view engine', 'ejs');
@@ -72,7 +80,6 @@ app.get('/categorie/:id', function (req, res) {
     con.query("SELECT * FROM produit WHERE produit_catégorie_id_catégorie = (SELECT id_catégorie FROM produit_catégorie WHERE nom = '" + req.params.id + "'); "+
     "SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC", [1,2],
         function (err, result) {
-            console.log(result);
             res.render('pages/categorie.ejs', {
                 siteTitle: siteTitle,
                 pageTitle: "Categorie",
@@ -86,7 +93,10 @@ app.get('/categorie/:id', function (req, res) {
 pour generer la page de produit
 */
 app.get('/produit/:id', function (req, res) {
-    con.query("SELECT image, marque, nom, prix, description FROM produit WHERE nom = '" + req.params.id + "'; SELECT b.nombre, c.nom, c.adresse, c.ville, c.code_postale, c.tel FROM produit a, inventaire b, magasin c WHERE a.id_produit = b.produit_id_produit AND a.nom = '" + req.params.id + "' AND b.magasin_id_magasin = c.id_magasin; SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC;",[1,2,3], 
+    con.query("SELECT image, marque, nom, prix, description FROM produit WHERE nom = '" + req.params.id + "';" +
+        " SELECT b.nombre, c.nom, c.adresse, c.ville, c.code_postale, c.tel FROM produit a, inventaire b, magasin c WHERE a.id_produit = b.produit_id_produit AND a.nom = '" 
+        + req.params.id + "' AND b.magasin_id_magasin = c.id_magasin;"+
+        " SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC;", 
         function (err, result) {
             res.render('pages/produit.ejs', {
                 siteTitle: siteTitle,
@@ -102,12 +112,15 @@ app.get('/produit/:id', function (req, res) {
 pour generer la page de panier
 */
 app.get('/panier', function (req, res) {
-    con.query("SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC",
+        con.query("SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC;" + 
+        " SELECT panier.nombre, produit.image, produit.nom, produit.marque FROM panier, produit WHERE utilisateur_id_utilisateur = ? AND panier.produit_id_produit = produit.id_produit;", 
+        [req.session.id_utilisateur],
         function (err, result) {
             res.render('pages/panier.ejs', {
                 siteTitle: siteTitle,
                 pageTitle: "Panier",
-                items: result
+                items: result[0],
+                outils: result[1]
             });
         });
 });
@@ -126,7 +139,27 @@ app.get('/creation', function (req, res) {
         });
 });
 
-
+/**
+ * Reception de connexion et mise en memoire
+ */
+app.post('/connexion', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    if (username && password) {
+        con.query('SELECT * FROM utilisateur WHERE email = ? AND mot_de_passe = ?', [username, password], function(error, results, fields) {
+            if (results.length > 0) {
+                req.session.loggedin = true;
+                req.session.username = username;
+                req.session.id_utilisateur = results[0].id_utilisateur;
+                res.redirect('/panier');
+            } else {
+                res.redirect('/connexion');
+            }
+        });
+    } else {
+        res.redirect('/connexion');
+    }
+});
 
 /**
  * post methode to date : pour ajouter un utilisateur a la BD
