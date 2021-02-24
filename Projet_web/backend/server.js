10d123503aefd5ac7cdfd2e4a48c7678b9a8a7ce
Diego Felipe Duran Lezama <diegoduran2001@gmail.com>
2021-02-24 15:35:32 -0500

    modification de code

--- Projet_web/backend/server.js
+++ Projet_web/backend/server.js
@@ -176,6 +176,29 @@ app.post('/connexion', function(req, res) {
 });
 
 /**
+ * get methode : poure fermer la session de l'utilisateur
+ */
+app.get('/logout',  function (req, res, next)  {
+    if (req.session) {
+      // delete session object
+      req.session.destroy(function (err) {
+         
+        if (err) {
+           next(err);
+        } else {
+           
+           res.redirect('/');
+        }
+         
+      });
+    }
+  });
+
+
+
+
+
+/**
  * post methode to date : pour ajouter un utilisateur a la BD
  */
 app.post('/creation', function (req,res){
